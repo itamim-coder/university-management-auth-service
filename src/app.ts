54a1fb99631108e384da5db7324930ee378be0f3
itamim-coder <itamim12202@gmail.com>
2023-06-03 12:51:18 +0600

    user services added

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
 //parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//application routes
+
+app.use('/api/v1/users/', usersRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully')
